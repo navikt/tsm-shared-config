ecb1652af4f3a5d201543eb6827ee512c8f1f2b1
Karl J. Overå <k@rl.run>
2026-07-08 11:37:00 +0200

    chore: add jsx-a11y as default to react preset (oxlint)

--- libs/oxlint/react.config.js
+++ libs/oxlint/react.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'oxlint'
 
 export default defineConfig({
-    plugins: ['react', 'react-perf'],
+    plugins: ['react', 'react-perf', 'jsx-a11y'],
     rules: {
         'react/rules-of-hooks': 'error',
         'react/jsx-curly-brace-presence': 'warn',
